2ee774cb2973218bb7930355599537d3fc29b6e0
lbias <306508354@qq.com>
2017-05-25 23:05:29 +0800

    routes for products, reset root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'products#index'
 
   namespace :admin do
     resources :products
-  end  
+  end
+
+  resources :products
 end
