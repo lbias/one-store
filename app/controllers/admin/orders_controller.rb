6bef18b6ca02f4fb43ab31b1eab61d9d12471712
lbias <306508354@qq.com>
2017-06-01 10:35:09 +0800

    nofity_cancel in controller admin::orders#cancel

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -29,6 +29,7 @@ class Admin::OrdersController < ApplicationController
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
