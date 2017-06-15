a024951f32789a9c4685577b0a8996385685ee11
lbias <306508354@qq.com>
2017-06-15 14:50:34 +0800

    add name, nickname, address, phone, birthday, gender to table users

--- db/migrate/20170615064809_add_columns_to_users.rb
+++ db/migrate/20170615064809_add_columns_to_users.rb
@@ -1,4 +1,10 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.0]
   def change
+    add_column :users, :name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :address, :string
+    add_column :users, :phone, :string
+    add_column :users, :birthday, :date
+    add_column :users, :gender, :string
   end
 end
