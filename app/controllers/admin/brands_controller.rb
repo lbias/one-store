3166d21e3cb6d28b157bc8d2c4b249ac29b55adb
lbias <306508354@qq.com>
2017-06-15 17:59:09 +0800

    controller admin::brands#index, show, new, create, edit, update, publish, hide

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,2 +1,62 @@
 class Admin::BrandsController < ApplicationController
+  # 用户必须登录
+  before_action :authenticate_user!
+  # 用户必须是 admin 身份
+  before_action :admin_required
+  # 后台页面布局
+  layout "admin"
+
+  def index
+    @brands = Brand.all)
+  end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+      redirect_to admin_brands_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+    if @brand.update(brand_params)
+      redirect_to admin_brands_path
+    else
+      render :edit
+    end
+  end
+
+  # 发布
+  def publish
+    @brand = Brand.find(params[:id])
+    @brand.publish!
+    redirect_to :back
+  end
+
+  # 隐藏
+  def hide
+    @brand = Brand.find(params[:id])
+    @brand.hide!
+    redirect_to :back
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :description, :logo, :is_hidden)
+  end
 end
