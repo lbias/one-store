c8cacb503a0615258101503a430293df62e0788a
lbias <306508354@qq.com>
2017-06-15 17:23:43 +0800

    add details to table brands, categories, category_groups

--- db/migrate/20170615092036_create_category_groups.rb
+++ db/migrate/20170615092036_create_category_groups.rb
@@ -1,7 +1,8 @@
 class CreateCategoryGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :category_groups do |t|
-
+      t.string :name
+      t.boolean :is_hidden, default: false
       t.timestamps
     end
   end
