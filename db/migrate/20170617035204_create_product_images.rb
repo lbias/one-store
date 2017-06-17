2851b63389e9a28b9e6b396bb15d43b425ed00fb
lbias <306508354@qq.com>
2017-06-17 11:54:38 +0800

    add product_id, image, main_image to table product_images

--- db/migrate/20170617035204_create_product_images.rb
+++ db/migrate/20170617035204_create_product_images.rb
@@ -1,7 +1,9 @@
 class CreateProductImages < ActiveRecord::Migration[5.0]
   def change
     create_table :product_images do |t|
-
+      t.integer :product_id
+      t.string :image
+      t.boolean :main_image, default: false
       t.timestamps
     end
   end
