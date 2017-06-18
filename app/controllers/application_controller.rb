a9e2bc4f909d0469ad66c2597c0b15d635016da5
lbias <306508354@qq.com>
2017-06-18 21:06:17 +0800

    controller application#set_currency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  before_action :set_currency
   protect_from_forgery with: :exception
 
   def admin_required
     if !current_user.admin?
-      redirect_to "/", alert: "You are not admin."
+      redirect_to "/", alert: t('warning-not-admin')
     end
   end
 
@@ -11,6 +13,19 @@ class ApplicationController < ActionController::Base
 
   def current_cart
     @current_cart ||= find_cart
+  end
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  def set_currency
+    if params[:currency]
+      session[:currency] = params[:currency]
+    end
   end
 
   private
@@ -22,5 +37,5 @@ class ApplicationController < ActionController::Base
     end
     session[:cart_id] = cart.id
     return cart
-  end  
+  end
 end
