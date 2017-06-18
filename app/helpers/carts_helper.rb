43b444fb2a3c86c545f99d9326486b2f4def1bc3
lbias <306508354@qq.com>
2017-06-18 19:26:35 +0800

    helper carts#render_product_quantity

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,4 +2,10 @@ module CartsHelper
   def render_cart_total_price(cart)
     cart.total_price
   end
+
+  def render_product_quantity(product)
+    @product = Product.find(product)
+    @orderSum = OrderItem.where("product_id" => product).sum(:quantity)
+    @product.quantity - @orderSum
+  end  
 end
