b819f4787e1e5255926eb84d070b4cb9ce2c307a
lbias <306508354@qq.com>
2017-06-18 20:41:59 +0800

    add name, symbol, rate to table currencies

--- db/migrate/20170618123953_create_currencies.rb
+++ db/migrate/20170618123953_create_currencies.rb
@@ -1,7 +1,9 @@
 class CreateCurrencies < ActiveRecord::Migration[5.0]
   def change
     create_table :currencies do |t|
-
+      t.string :name
+      t.string :symbol
+      t.float :rate
       t.timestamps
     end
   end
