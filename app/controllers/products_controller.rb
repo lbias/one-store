c8d06e21b513c8af8717e3b9277996686c8524bb
lbias <306508354@qq.com>
2017-06-18 14:02:37 +0800

    scope recent

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,23 +8,23 @@ class ProductsController < ApplicationController
     if params[:category].present?
       @category_s = params[:category]
       @category = Category.find_by(name: @category_s)
-      @products = Product.where(:category => @category.id).published.paginate(:page => params[:page], :per_page => 12)
+      @products = Product.where(:category => @category.id).published.recent.paginate(:page => params[:page], :per_page => 12)
 
     # 判斷是否筛选大类
     elsif params[:group].present?
       @group_s = params[:group]
       @group = CategoryGroup.find_by(name: @group_s)
-      @products = Product.joins(:category).where("categories.category_group_id" => @group.id).published.paginate(:page => params[:page], :per_page => 12)
+      @products = Product.joins(:category).where("categories.category_group_id" => @group.id).published.recent.paginate(:page => params[:page], :per_page => 12)
 
     # 判斷是否筛选品牌
     elsif params[:brand].present?
       @brand_s = params[:brand]
       @brand = Brand.find_by(name: @brand_s)
-      @products = Product.where(:brand => @brand.id).published.paginate(:page => params[:page], :per_page => 12)
+      @products = Product.where(:brand => @brand.id).published.recent.paginate(:page => params[:page], :per_page => 12)
 
     # 默认显示所有公开发布的商品
     else
-      @products = Product.published.paginate(:page => params[:page], :per_page => 12)
+      @products = Product.published.recent.paginate(:page => params[:page], :per_page => 12)
     end
   end
 
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
     @product_stock = @product.quantity - @orderSum
 
     # 大类 / 品牌
-    @category_groups = CategoryGroup.published.paginate(:page => params[:page], :per_page => 12)
+    @category_groups = CategoryGroup.published.recent.paginate(:page => params[:page], :per_page => 12)
     @brands = Brand.published
 
     set_page_title       @product.title
