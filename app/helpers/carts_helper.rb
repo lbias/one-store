0c1e931ec4927427f0c4a08d8142a9637bf71a07
lbias <306508354@qq.com>
2017-06-18 20:49:50 +0800

    helper carts#render_cart_total_price

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,11 +1,17 @@
 module CartsHelper
   def render_cart_total_price(cart)
-    cart.total_price
+    if session[:currency].present? # 搜寻币值数据
+      @currency_s = session[:currency]
+      @currency = Currency.find_by(name: @currency_s)
+      @currency.symbol + " " + cart.total_price.to_s
+    else # 预设币值为新币
+      "$ " + cart.total_price.to_s
+    end
   end
 
   def render_product_quantity(product)
     @product = Product.find(product)
     @orderSum = OrderItem.where("product_id" => product).sum(:quantity)
     @product.quantity - @orderSum
-  end  
+  end
 end
