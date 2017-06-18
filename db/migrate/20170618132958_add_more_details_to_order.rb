7149e6c001a7f8bade05e92230fbb0c55dab01be
lbias <306508354@qq.com>
2017-06-18 21:34:23 +0800

    add order_id, shipping_phone, currency to table orders

--- db/migrate/20170618132958_add_more_details_to_order.rb
+++ db/migrate/20170618132958_add_more_details_to_order.rb
@@ -1,4 +1,7 @@
 class AddMoreDetailsToOrder < ActiveRecord::Migration[5.0]
   def change
+    add_column :orders, :order_id, :integer
+    add_column :orders, :shipping_phone, :string
+    add_column :orders, :currency, :string
   end
 end
