09f8c95cee77b89f3aebe78cd1a366cf93cdb4c0
lbias <306508354@qq.com>
2017-06-18 20:37:03 +0800

    change product quantity in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,73 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require_tree .
+
+
+// Products#show - 变更显示图片
+$(document).on('mouseover', '.list-image', function () {
+  var src_other = $(this).attr('src') //抓取小图图片路径
+  var src_main = src_other.toString().replace("other", "main") //更改图片路径
+  $('.main-image').attr('src', src_main) //变更大图图片路径
+  $('.list-image').removeClass('list-image-active') //其它图片移除锁定状态
+  $(this).addClass('list-image-active') //当前图片新增锁定状态
+})
+
+// Products#show - 变更购买数量
+$(document).on('turbolinks:load', function() {
+  // 增加购买数量
+  $("#quantity-up").click(function(e) {
+    var num = parseInt($("#quantity").val());
+    var numMax = $("#quantity").attr("max");
+    if (num < numMax) {
+      $("#quantity").val(num += 1);
+    }
+    e.preventDefault();
+  });
+
+  // 减少购买数量
+  $("#quantity-down").click(function(e) {
+    var num = parseInt($("#quantity").val());
+    if (num > 1) {
+      $("#quantity").val(num -= 1);
+    }
+    e.preventDefault();
+  });
+
+  // 确认购买数量，不能超过库存
+  $("#quantity").blur(function(e) {
+    var num = parseInt($(this).val());
+    var numMax = $(this).attr("max");
+    if (num > numMax) {
+      num = numMax;
+    } else if (num < 0) {
+      num = 1
+    }
+    $(this).val(num);
+    e.preventDefault();
+  });
+
+  // 确认购买数量，不能超过库存
+  $(".cart-quantity-input").blur(function(e) {
+    var num = parseInt($(this).val());
+    var numMax = $(this).attr("max");
+    if (num > numMax) {
+      num = numMax;
+    } else if (num < 0) {
+      num = 1
+    }
+    $(this).val(num);
+    e.preventDefault();
+
+    var id = $(this).attr("id");
+    $.ajax({
+      type: "PATCH",
+      url: "/cart_items/"+id,
+        dataType:'json',
+        data:{
+          quantity: num
+        },
+    });
+
+    window.location.reload();
+  });
+});
