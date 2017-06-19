54e49eea5c4bf5564d49ee4e6c1f8f53a1663554
lbias <306508354@qq.com>
2017-06-19 13:45:24 +0800

    helper intros#render_intro_status, render_intro_publish_or_hide

--- app/helpers/admin/intros_helper.rb
+++ app/helpers/admin/intros_helper.rb
@@ -1,2 +1,17 @@
 module Admin::IntrosHelper
+  def render_intro_status(intro)
+    if intro.is_hidden
+      content_tag(:span, "", :class => "fa fa-lock fa-fw")
+    else
+      content_tag(:span, "", :class => "fa fa-globe fa-fw")
+    end
+  end
+
+  def render_intro_publish_or_hide(intro)
+    if intro.is_hidden
+      link_to(t('btn-publish'), publish_admin_intro_path(intro), :method => :post, :class => 'btn btn-sm btn-default')
+    else
+      link_to(t('btn-hide'), hide_admin_intro_path(intro), :method => :post, :class => 'btn btn-sm btn-default')
+    end
+  end
 end
