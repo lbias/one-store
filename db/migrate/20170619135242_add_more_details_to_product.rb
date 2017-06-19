78c5d98e48691eb10688d7f270f065f0ac639296
lbias <306508354@qq.com>
2017-06-19 21:55:12 +0800

    add size, material, country, is_chosen to table products

--- db/migrate/20170619135242_add_more_details_to_product.rb
+++ db/migrate/20170619135242_add_more_details_to_product.rb
@@ -1,4 +1,8 @@
 class AddMoreDetailsToProduct < ActiveRecord::Migration[5.0]
   def change
+    add_column :products, :size, :string
+    add_column :products, :material, :string
+    add_column :products, :country, :string
+    add_column :products, :is_chosen, :boolean, default: false        
   end
 end
